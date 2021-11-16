4aeaa4984cb367b6187b3462900e35c7bcb65708
Mason <mjhawks721@gmail.com>
2021-11-16 15:24:41 -0600

    Added blocking sparks only facing left

--- Assets/Bandits - Pixel Art/Scripts/SparkEffect.cs
+++ Assets/Bandits - Pixel Art/Scripts/SparkEffect.cs
@@ -7,16 +7,30 @@ public class SparkEffect : MonoBehaviour
     Transform block;
     Transform attack;
 
+
     // Start is called before the first frame update
     void Start()
     {
         block  = transform.Find("Block");
         attack = transform.Find("Attack");
+        var emit = block.GetComponent<ParticleSystem>().emission;
+        emit.enabled = false;
+
     }
 
     public void EmitBlockSparks()
     {
+        Debug.Log("emitting particles");
+        var emit = block.GetComponent<ParticleSystem>().emission;
+        emit.enabled = true;
+        StartCoroutine(stopBlockSparks());
         return;
+    }
+    IEnumerator stopBlockSparks()
+    {
+        yield return new WaitForSeconds(.4f);
+        var emit = block.GetComponent<ParticleSystem>().emission;
+        emit.enabled = false;
     }
 
     public void EmitParrySparks()
