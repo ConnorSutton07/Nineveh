b27d16f0eb3a22c47e573d8bf3d72d5d05fbd63b
Connor <connorsutton07@gmail.com>
2022-01-24 13:31:34 -0600

    ranged enemy scripts

---
+++ Assets/Scripts/RangedEnemy/Arrow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+  public float speed;
+  public float lifetime;
+  public float distance;
+  public int damage;
+  public LayerMask whatIsSolid;
+
+  //public GameObject destroyEffect
+
+  private void Start()
+  {
+    Invoke("DestroyArrow", lifetime);
+  }
+  // Update is called once per frame
+  private void Update()
+  {
+    //want to change it such that it goes straight where aimed using transform.up 
+    //for raycast and translate
+    RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -transform.right, distance, whatIsSolid);
+    if (hitInfo.collider != null)
+    {
+      if (hitInfo.collider.CompareTag("Player"))
+      {
+        Debug.Log("PLAYER MUST TAKE DAMAGE!");
+        hitInfo.collider.GetComponent<Bandit>().TakeDamage(damage, 0, false);
+      }
+      DestroyArrow();
+    }
+    transform.Translate(-transform.right * speed * Time.deltaTime);
+  }
+
+  private void DestroyArrow()
+  {
+    //Instantiate(destroyEffect, transform.position, Quaternion.identity)
+    Destroy(gameObject);
+  }
+}
